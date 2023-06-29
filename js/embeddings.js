b567b3ad3f0e7c79a1dad0dd850d0b3b49a376bb
satellitecomponent <129367899+satellitecomponent@users.noreply.github.com>
2023-06-28 21:51:46 -0400

    deploy fix

--- js/embeddings.js
+++ js/embeddings.js
@@ -1,19 +1,19 @@
-import { pipeline } from 'https://cdn.jsdelivr.net/npm/@xenova/transformers@2.3.0';
+//import { pipeline } from 'https://cdn.jsdelivr.net/npm/@xenova/transformers@2.3.0';
 
-let modelInitialized = false;
+//let modelInitialized = false;
 
-async function initialize() {
-    try {
-        window.generateEmbeddings = await pipeline(
-            'feature-extraction',
-            'Xenova/all-MiniLM-L6-v2'
-        );
-        modelInitialized = true;
-        document.getElementById("local-embeddings-checkbox").disabled = false;
-    } catch (error) {
-        console.error('Error initializing the model:', error);
-    }
-}
+//async function initialize() {
+  //  try {
+ //       window.generateEmbeddings = await pipeline(
+  //          'feature-extraction',
+  //          'Xenova/all-MiniLM-L6-v2'
+  //      );
+  //      modelInitialized = true;
+  //      document.getElementById("local-embeddings-checkbox").disabled = false;
+  //  } catch (error) {
+  //      console.error('Error initializing the model:', error);
+ //   }
+//}
 
-document.getElementById("local-embeddings-checkbox").disabled = true;
-initialize();
+//document.getElementById("local-embeddings-checkbox").disabled = true;
+//initialize();
