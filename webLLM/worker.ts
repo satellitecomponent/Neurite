933fe550abf288468754ded87e33aebd2d1a5e53
satellitecomponent <129367899+satellitecomponent@users.noreply.github.com>
2023-07-11 13:15:16 -0400

    dist commit

--- webLLM/worker.ts
+++
@@ -1,9 +0,0 @@
-// worker.ts
-import { ChatWorkerHandler, ChatModule } from "@mlc-ai/web-llm";
-
-// Hookup a chat module to a worker handler
-const chat = new ChatModule();
-const handler = new ChatWorkerHandler(chat);
-self.onmessage = (msg: MessageEvent) => {
-    handler.onmessage(msg);
-};
