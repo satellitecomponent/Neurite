d7e6cce96f62eb742c16948213904e7bbb949e0a
satellitecomponent <129367899+satellitecomponent@users.noreply.github.com>
2025-02-08 18:55:37 -0800

    macos traffic lights

--- modules/windowmanager.js
+++ modules/windowmanager.js
@@ -5,6 +5,8 @@ const { buildContextMenu } = require('./contextmenu');
 function createMainWindow() {
     Menu.setApplicationMenu(null);
 
+    const isMac = process.platform === 'darwin'; // Check if macOS
+
     const mainWindow = new BrowserWindow({
         width: 1200,
         height: 800,
@@ -20,6 +22,10 @@ function createMainWindow() {
             preload: path.join(__dirname, "preload.js"),
         },
         icon: path.join(__dirname, '../build/icons/icon.png'),
+
+        // macOS-specific settings
+        titleBarStyle: isMac ? 'hiddenInset' : 'default', // Inset traffic lights on macOS
+        trafficLightPosition: isMac ? { x: 12, y: 10 } : undefined, // Adjust inset position
     });
 
     mainWindow.loadURL('https://neurite.network');
@@ -31,7 +37,7 @@ function createMainWindow() {
         menu.popup({ window: bw, x: params.x, y: params.y });
     });
 
-    //mainWindow.webContents.openDevTools();
+    // mainWindow.webContents.openDevTools();
     return mainWindow;
 }
 
